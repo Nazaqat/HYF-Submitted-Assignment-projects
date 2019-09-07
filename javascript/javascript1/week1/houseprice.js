8348e621935d8c0f983213fd29bba6bbca0af48c
Nazaqat <naa.dk@yahoo.com>
2019-09-07 21:15:19 +0200

    updated

--- javascript/javascript1/week1/houseprice.js
+++ javascript/javascript1/week1/houseprice.js
@@ -1,6 +1,9 @@
+
 const housePrice = [2500000, 1000000];
 const volumeInMeters = [[8*10*10], [5*11*8]];
 const gardenSizeInM2 = [100, 72];
+
+
 
 PetersHousePrice = volumeInMeters[0] * 2.5 * 1000 + gardenSizeInM2[0] * 300;
 console.log(`The peters house cost ${PetersHousePrice} but he is paying ${housePrice[0]}, means ${housePrice[0] - PetersHousePrice} over market rate`);
@@ -8,6 +11,15 @@ console.log(`The peters house cost ${PetersHousePrice} but he is paying ${houseP
 
 JuliaHousePrice = volumeInMeters[1] * 2.5 * 1000 + gardenSizeInM2[1] * 300;
 console.log(`The Julia house cost ${JuliaHousePrice} but she is paying ${housePrice[1]}, means ${housePrice[1] - JuliaHousePrice} less than market rate`);
+
+
+
+function CalculatePriceCalculation(volumeInMeters, gardenSizeInM2); // same results can be drawn with function 
+{
+return volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300; 
+}
+
+
 
 
 
