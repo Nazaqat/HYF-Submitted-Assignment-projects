17674c5cb71404e26cb7526313d642a4920b6c85
Nazaqat <naa.dk@yahoo.com>
2019-09-13 18:00:07 +0200

    weather wear

---
+++ javascript/javascript1/week2/weather.js
@@ -0,0 +1,28 @@
+
+
+function wvear(temperature){
+	if (temperature >= 40){
+	return "shorts and T-shirt will do"
+}
+
+	else if (temperature >= 20){
+	return "Very shorts and T-shirt will do"
+}
+	else if (temperature >= 0){
+	return "big boy pants get a sweater and put a heavy coat on"
+}
+	else if (temperature <= -10){
+	return "Stay inside you fool theres no need to freeze to death"
+}
+	else (temperature >= -20)
+	return "Dont mess with weather"
+}
+
+
+
+
+const temperature = wvear(5);
+console.log(temperature);
+
+
+
