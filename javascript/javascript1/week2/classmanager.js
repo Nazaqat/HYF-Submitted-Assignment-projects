714347162856837dd094ac10402af4ecf93b2f6c
Nazaqat <naa.dk@yahoo.com>
2019-09-14 04:32:08 +0200

    Add files via upload

--- javascript/javascript1/week2/classmanager.js
+++ javascript/javascript1/week2/classmanager.js
@@ -3,9 +3,13 @@ const class07Students = [];
 	function addStudentToClass(studentName) {
 	
 	if (class07Students.length <= 6 || studentName === "Queen"){
-	return class07Students.push(studentName) && "Please Enter more students";
+	return class07Students.push(studentName);
 	}
 
+	else if(class07Students.length <6){
+	return "Please Enter more students"
+	}
+	
 	else if( class07Students.length === 6 && studentName !== "Queen"){
 		return "No more students can be added to class 07"
 	}
