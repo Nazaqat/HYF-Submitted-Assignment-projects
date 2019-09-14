65b5b406ad25123310c2701750f16ab0ec9fb1e2
Nazaqat <naa.dk@yahoo.com>
2019-09-14 04:27:37 +0200

    class manager

---
+++ javascript/javascript1/week2/classmanager.js
@@ -0,0 +1,40 @@
+
+const class07Students = [];
+	function addStudentToClass(studentName) {
+	
+	if (class07Students.length <= 6 || studentName === "Queen"){
+	return class07Students.push(studentName) && "Please Enter more students";
+	}
+
+	else if( class07Students.length === 6 && studentName !== "Queen"){
+		return "No more students can be added to class 07"
+	}
+
+	else if (class07Students.includes(studentName)){
+		return "student ${studentName} exists already"
+	}
+
+		
+}
+
+
+addStudentToClass("Anders");
+addStudentToClass("Heidi");
+addStudentToClass("Kamran");
+addStudentToClass("Touseef");
+addStudentToClass("Najmi");
+addStudentToClass("Gitte");
+addStudentToClass("Queen");
+
+console.log(class07Students);
+console.log(class07Students.length);
+
+
+	
+
+
+
+
+
+
+
