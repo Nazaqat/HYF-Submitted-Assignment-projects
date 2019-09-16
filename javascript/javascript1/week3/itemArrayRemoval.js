f50528108542c21358ab4099b0ae2a861c689a82
Nazaqat <naa.dk@yahoo.com>
2019-09-16 23:52:06 +0200

    itemArrayRemoval

--- javascript/javascript1/week3/itemArrayRemoval.js
+++ javascript/javascript1/week3/itemArrayRemoval.js
@@ -1,7 +1,8 @@
 const names = ['Peter', 'Ahmad', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala'];
 const nameToRemove = 'Ahmad';
 
-const removed = names.splice(1,1);
+const removed = names.splice(1,1);//The first argument specifies the location at which to begin adding or removing elements. 
+//The second argument specifies the number of elements to remove.
 
 console.log(names);
 
