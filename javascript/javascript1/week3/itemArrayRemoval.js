e9de174cc49e1f6ed672d3e8f49d2b01f3dc500e
Nazaqat <naa.dk@yahoo.com>
2019-09-16 23:49:47 +0200

    itemArrayRemoval

---
+++ javascript/javascript1/week3/itemArrayRemoval.js
@@ -0,0 +1,12 @@
+const names = ['Peter', 'Ahmad', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala'];
+const nameToRemove = 'Ahmad';
+
+const removed = names.splice(1,1);
+
+console.log(names);
+
+// // names.length = 2; //to show array restricted to second value
+// // names.pop(); // to delete value from last of array
+// // names.shift(); // delete value from begning
+
+
