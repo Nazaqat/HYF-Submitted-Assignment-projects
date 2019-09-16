61d7e78095423b9aa98f86b405aa62b607363a4b
Nazaqat <naa.dk@yahoo.com>
2019-09-17 01:38:35 +0200

    travelTime

---
+++ javascript/javascript1/week3/travelTime.js
@@ -0,0 +1,18 @@
+const travelInformation = {
+    speed: 50,
+    destinationDistance: 432,
+  };
+  
+  const travelTime = travelInformation[`destinationDistance`]/travelInformation[`speed`]*[60];
+
+  // total time in minutes ll be displayed as hours and minutes
+  function timeConvert(n) {
+    var num = n;
+    var hours = (num / 60);
+    var rhours = Math.floor(hours);
+    var minutes = (hours - rhours) * 60;
+    var rminutes = Math.round(minutes);
+    return "Time to destination is" +" " + num + " minutes OR " + rhours + " hour(s) and " + rminutes + " minute(s).";
+    }
+    console.log(timeConvert(travelTime));
+
