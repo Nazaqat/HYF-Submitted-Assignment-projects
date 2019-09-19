571ed74330551a4d7c862fc1c4698c04d4177d82
Nazaqat <naa.dk@yahoo.com>
2019-09-20 00:25:19 +0200

    init

--- javascript/javascript1/week3/lifeDuration.js
+++ javascript/javascript1/week3/lifeDuration.js
@@ -5,14 +5,14 @@ const theWire = (2.50/29200*100);
 
 function getWholePercent1(percentOf,percentFor){
 for (let i=0; i<3; i++){
-document.write (percentFor/percentOf*100);
+document.write(percentFor/percentOf*100);
 
 }
 }
 
-console.log("The Game of Thrones took" + " " + gameOfThrones + " " + "of my precious life");
-console.log("Sopranos took" + " " + sopranos + " " + "of my precious life");
-console.log("The Wire took" + " " + theWire + " " + "of my precious life");
-console.log("In total that is" + " " + (gameOfThrones + sopranos + theWire) + " "+"of my life");
+console.log("The Game of Thrones took" + " " + gameOfThrones.toFixed(3)  +  " " + "of my precious life");
+console.log("Sopranos took" + " " + sopranos.toFixed(2)  + " " + "of my precious life");
+console.log("The Wire took" + " " + theWire.toFixed(3)  + " " + "of my precious life");
+console.log("In total that is" + " " + (gameOfThrones + sopranos + theWire).toFixed(3)  + " "+"of my life");
 
 
