fefc28f2abc43dca9b942389b3d123815e5a42ce
Nazaqat <naa.dk@yahoo.com>
2019-10-13 09:55:42 +0200

    code reconsidered

--- javascript/javascript2/week2/doublingOfNumbers.js
+++ javascript/javascript2/week2/doublingOfNumbers.js
@@ -1,9 +1,5 @@
 const numbers = [1, 2, 3, 4];
-const oddNumbers = numbers.filter(items => items % 2 === 1);// console.log(oddNumbers);
-
-const newNumbers = oddNumbers.map(items=>{
-    if (items % 2 === 1)
-       return (items * 2)
-});
+const newNumbers = numbers.filter(items => items % 2 === 1).map(items=>items * 2);
 
 console.log("The doubled numbers are", newNumbers);// returns [2, 6]
+
