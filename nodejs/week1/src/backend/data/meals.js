044dfc92d145053f4c4295258641a19eb9504505
Nazaqat <naa.dk@yahoo.com>
2019-12-05 23:34:57 +0100

    commit init

---
+++ nodejs/week1/src/backend/data/meals.js
@@ -0,0 +1,33 @@
+[{
+        "id": 1,
+        "title": "Indian food in the summer",
+        "maxNumberOfGuests": 5,
+        "description": "A nice night out eating delicious indian food",
+        "createdAt": "2019/12/7 14:34",
+        "price": 67
+    },
+    {
+        "id": 2,
+        "title": "Indian food in the winter",
+        "maxNumberOfGuests": 17,
+        "description": "A nice night out eating delicious indian food",
+        "createdAt": "2019/12/7 14:34",
+        "price": 100
+    },
+    {
+        "id": 3,
+        "title": "Indian food in the lunch",
+        "maxNumberOfGuests": 90,
+        "description": "A nice night out eating delicious indian food",
+        "createdAt": "2019/12/7 14:34",
+        "price": 80
+    },
+    {
+        "id": 4,
+        "title": "Indian food in the summer",
+        "maxNumberOfGuests": 35,
+        "description": "A nice night out eating delicious indian food",
+        "createdAt": "2019/12/7 14:34",
+        "price": 110
+    }
+]
