6140a4df7cec6c70dfd3dd4196b13925573afb31
Nazaqat <naa.dk@yahoo.com>
2019-12-05 23:36:27 +0100

    commit init

---
+++ nodejs/week1/src/backend/data/reviews.js
@@ -0,0 +1,30 @@
+[{
+        "id": 1,
+        "numberOfStars": 3,
+        "content": "Best meal ever",
+        "mealId": 1,
+        "createdAt": 12345
+    },
+    {
+        "id": 2,
+        "numberOfStars": 5,
+        "content": "Best meal ever",
+        "mealId": 7,
+        "createdAt": 12345
+    },
+    {
+        "id": 1,
+        "numberOfStars": 7,
+        "content": "Best meal ever",
+        "mealId": 3,
+        "createdAt": 12345
+    },
+    {
+        "id": 1,
+        "numberOfStars": 4,
+        "content": "Best meal ever",
+        "mealId": 6,
+        "createdAt": 12345
+    }
+
+]
